dd2abd37f60df91be4e8637a26f9de1bb10bf166
klaudia <klaudiamiekina11@gmail.com>
2022-06-17 22:59:28 +0200

    17.06 parcels_export.py

--- parcels_export.py
+++ parcels_export.py
@@ -1,10 +1,29 @@
 # klaudia
 from parcel_dto import *
+import json
+from my_deque import Deque
+
 
 class ParcelsExport:
 
-    def __init__(self, TruckClass):
+    def __init__(self):
+        self.d = Deque()
 
-        for index in TruckClass:
-            pass
+    def import_json(self, file_name="truck.json"):
+        with open(file_name) as trucks_data:
+            trucks = json.load(trucks_data)
+            trucks_data.close()
+        return trucks
+
+    def warehouse_parcel(self):
+        '''pobieranie paczki z magazynu'''
+        pass
+
+    def truck_parcel(self):
+        '''wkladanie paczki z magazynu do ciezarowki'''
+        pass
+
+p = ParcelsExport()
+print(p.import_json())
+
 
