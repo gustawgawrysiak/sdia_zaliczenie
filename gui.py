ce3bbb317bbd1b8a98121d989434e4b36bc429c2
klaudia <klaudiamiekina11@gmail.com>
2022-06-26 21:57:15 +0200

    gui.py zmiany w metodach load_*, skrypt uruchamiany jest przy wczytaniu dwoch plikow

--- gui.py
+++ gui.py
@@ -13,8 +13,10 @@ QRadioButton,
 QFileDialog,
 QPushButton,
 QLabel,
-QLineEdit
+QLineEdit,
+QButtonGroup
 )
+from functools import partial
 from warehouse import *
 from parcels_export import ParcelsExport
 # from PyQt5.QtGui import *
@@ -27,7 +29,7 @@ class MyWindow(QMainWindow):
         """ustawienie minimalnych wymiarów okna dialogowego
         oraz tytułu okna"""
 
-        self.setMinimumHeight(30)
+        self.setMinimumHeight(120)
         self.setMinimumWidth(380)
 
         self.window_title(window_name)
@@ -43,48 +45,55 @@ class MyWindow(QMainWindow):
         lbl_parcels.setAlignment(QtCore.Qt.AlignCenter)
         self.layout.addWidget(lbl_parcels, 0, 0)
 
-        btn_load_parcels = QPushButton('Wczytaj plik z paczkami')
-        self.layout.addWidget(btn_load_parcels, 2, 0, 2, 0)
-        btn_load_parcels.clicked.connect(self.load_parcels_json)
+        self.btn_load_parcels = QPushButton('Wczytaj plik z paczkami')
+        self.layout.addWidget(self.btn_load_parcels, 2, 0, 2, 0)
+        self.btn_load_parcels.clicked.connect(self.load_parcels_json)
 
-        btn_load_trucks = QPushButton('Wczytaj plik z ciężarówkami')
-        self.layout.addWidget(btn_load_trucks, 4, 0, 2, 0)
-        btn_load_trucks.clicked.connect(self.load_trucks_json)
+        self.btn_load_trucks = QPushButton('Wczytaj plik z ciężarówkami')
+        self.layout.addWidget(self.btn_load_trucks, 4, 0, 2, 0)
+        self.btn_load_trucks.clicked.connect(self.load_trucks_json)
 
         container.setLayout(self.layout)
 
     def window_title(self, tytul):
         self.setWindowTitle(tytul)
 
-    def load_parcels_json(self):
-        self.file = QFileDialog.getOpenFileName(self, 'Wskaż plik json', '', 'Wszystkie pliki (*.*);; Plik json (*.json)', 'Plik json (*.json)')
+    def load_parcels_json(self, value):
+        file = QFileDialog.getOpenFileName(self, 'Wskaż plik json', '', 'Wszystkie pliki (*.*);; Plik json (*.json)', 'Plik json (*.json)')
         try:
-            parcels = ParcelsImport.import_json(self.file[0])
-            return parcels
+            path = os.path.normpath(file[0])
+            self.file_parcels = path.split(os.sep)[-1]
+            ParcelsImport.import_json(self.file_parcels)
+            if self.file_parcels and self.file_trucks:
+                self.check_json_structure()
         except JSONDecodeError:
             lbl_json = QLabel('Wskazano plik o formacie innym niż json. Proszę spróbować ponownie.')
             lbl_json.setAlignment(QtCore.Qt.AlignCenter)
             self.layout.addWidget(lbl_json, 1, 0)
         except FileNotFoundError:
+            pass
+        except:
             pass
 
     def load_trucks_json(self):
-        self.file = QFileDialog.getOpenFileName(self, 'Wskaż plik json', '', 'Wszystkie pliki (*.*);; Plik json (*.json)', 'Plik json (*.json)')
+        file = QFileDialog.getOpenFileName(self, 'Wskaż plik json', '', 'Wszystkie pliki (*.*);; Plik json (*.json)', 'Plik json (*.json)')
         try:
-            if ParcelsExport.import_json(self.file[0]) and self.load_parcels_json:
-                # uruchomienie sryptu
-                self.layout.addWidget(QLabel(str(self.load_parcels_json)))
+            path = os.path.normpath(file[0])
+            self.file_trucks = path.split(os.sep)[-1]
+            ParcelsExport.import_json(self.file_trucks)
+            if self.file_trucks and self.file_parcels:
+                self.check_json_structure()
         except JSONDecodeError:
             lbl_json = QLabel('Wskazano plik o formacie innym niż json. Proszę spróbować ponownie.')
             lbl_json.setAlignment(QtCore.Qt.AlignCenter)
             self.layout.addWidget(lbl_json, 1, 0)
         except FileNotFoundError:
             pass
+        except:
+            pass
 
-    def script(self, parcels=None, trucks=None):
-        # raise NotImplementedError
-        pass
-
+    def check_json_structure(self):
+        raise NotImplementedError
 
 
 
