ed84de19a8fb34de0f3aeed5644ee02aa0e649aa
klaudia <klaudiamiekina11@gmail.com>
2022-06-29 19:48:25 +0200

    gui.py jak user wybierze dwa pliki, czysci layout i dodaje statystyki - u mnie blad connect

--- gui.py
+++ gui.py
@@ -14,12 +14,7 @@ from PyQt5.QtWidgets import (
     QMessageBox,
     QVBoxLayout
 )
-from PyQt5.QtCore import QCoreApplication
-from warehouse import *
-from parcels_export import ParcelsExport
-
-
-# from PyQt5.QtGui import *
+from main import run
 
 class FirstWindow(QMainWindow):
 
@@ -29,8 +24,8 @@ class FirstWindow(QMainWindow):
         """ustawienie minimalnych wymiarów okna dialogowego
         oraz tytułu okna"""
 
-        self.setMinimumHeight(150)
-        self.setMinimumWidth(380)
+        self.setMinimumHeight(400)
+        self.setMinimumWidth(600)
 
         self.window_title(window_name)
         container = QWidget()
@@ -39,21 +34,22 @@ class FirstWindow(QMainWindow):
         """utworzenie layoutu oraz umieszczenie w nim
         wszystkich przycisków, linii edycji oraz etykiet"""
 
-        self.layout = QGridLayout()
+        self.layout = QVBoxLayout()
 
         self.lbl_parcels = QLabel('Aby rozpocząć pracę z programem wybierz 2 pliki .json z paczkami i ciężarówkami')
         self.lbl_parcels.setAlignment(QtCore.Qt.AlignCenter)
-        self.layout.addWidget(self.lbl_parcels, 0, 0)
+        self.layout.addWidget(self.lbl_parcels)
 
         self.btn_load_parcels = QPushButton('Wczytaj plik z paczkami')
-        self.layout.addWidget(self.btn_load_parcels, 4, 0, 2, 0)
+        self.layout.addWidget(self.btn_load_parcels)
         self.btn_load_parcels.clicked.connect(self.load_parcels_json)
 
         self.btn_load_trucks = QPushButton('Wczytaj plik z ciężarówkami')
-        self.layout.addWidget(self.btn_load_trucks, 6, 0, 2, 0)
+        self.layout.addWidget(self.btn_load_trucks)
         self.btn_load_trucks.clicked.connect(self.load_trucks_json)
 
-        self.second_window = SecondWindow('Statystyki')
+        self.file_trucks = ''
+        self.file_parcels = ''
 
         container.setLayout(self.layout)
 
@@ -61,80 +57,44 @@ class FirstWindow(QMainWindow):
         self.setWindowTitle(tytul)
 
     def load_parcels_json(self):
-        file = QFileDialog.getOpenFileName(self, 'Wskaż plik json', '',
-                                           'Wszystkie pliki (*.*);; Plik json (*.json)', 'Plik json (*.json)')
+        file = QFileDialog.getOpenFileName(self, 'Wskaż plik json', '', 'Plik json (*.json)', 'Plik json (*.json)')
         try:
             path = os.path.normpath(file[0])
             self.file_parcels = path.split(os.sep)[-1]
-            ParcelsImport.import_json(self.file_parcels)
             if self.file_parcels and self.file_trucks:
-                self.start()
-        except JSONDecodeError:
-            msgBox = QMessageBox()
-            msgBox.setIcon(QMessageBox.Warning)
-            msgBox.setText('Wskazano plik o formacie innym niż json. Proszę spróbować ponownie.')
-            msgBox.setWindowTitle('Warning')
-            msgBox.setStandardButtons(QMessageBox.Ok)
-            msgBox.exec()
+                self.start_btn()
         except FileNotFoundError:
-            pass
-        except:
             pass
 
     def load_trucks_json(self):
-        file = QFileDialog.getOpenFileName(self, 'Wskaż plik json', '',
-                                           'Wszystkie pliki (*.*);; Plik json (*.json)', 'Plik json (*.json)')
+        file = QFileDialog.getOpenFileName(self, 'Wskaż plik json', '', 'Plik json (*.json)', 'Plik json (*.json)')
         try:
             path = os.path.normpath(file[0])
             self.file_trucks = path.split(os.sep)[-1]
-            ParcelsExport.import_json(self.file_trucks)
             if self.file_trucks and self.file_parcels:
-                self.start()
-        except JSONDecodeError:
-            msgBox = QMessageBox()
-            msgBox.setIcon(QMessageBox.Warning)
-            msgBox.setText('Wskazano plik o formacie innym niż json. Proszę spróbować ponownie.')
-            msgBox.setWindowTitle('Warning')
-            msgBox.setStandardButtons(QMessageBox.Ok)
-            msgBox.exec()
+                self.start_btn()
+
         except FileNotFoundError:
             pass
-        except:
-            pass
+
+    def start_btn(self):
+        self.btn_start = QPushButton('Wciśnij, aby rozpocząć skrypt')
+        self.layout.addWidget(self.btn_start)
+        self.btn_start.clicked.connect(self.hide())
+
+    def hide(self):
+        self.lbl_parcels.hide()
+        self.btn_load_trucks.hide()
+        self.btn_load_parcels.hide()
+        self.btn_start.hide()
+        self.start()
 
     def start(self):
-        self.close()
-        if self.second_window.isVisible():
-            self.second_window.hide()
-        else:
-            self.second_window.show()
-
-class SecondWindow(QMainWindow):
-
-    def __init__(self, window_name):
-        super().__init__()
-        """ustawienie minimalnych wymiarów okna dialogowego
-                oraz tytułu okna"""
-
-        self.setMinimumHeight(150)
-        self.setMinimumWidth(380)
-
-        self.window_title(window_name)
-        container = QWidget()
-        self.setCentralWidget(container)
-
-        """utworzenie layoutu oraz umieszczenie w nim
-        wszystkich przycisków, linii edycji oraz etykiet"""
-
-        self.layout = QGridLayout()
-
-        '''dodanie etykiet, qlineedits ze statystykami'''
-
-        container.setLayout(self.layout)
-
-    def window_title(self, tytul):
-        self.setWindowTitle(tytul)
-
+        main = run()
+        self.lbl_main(QLabel(f'Całkowity czas wykonywania skryptu: {main.get("main_time")}'))
+        self.layout.addWidget(self.lbl_main)
+        self.lbl_warehouse(QLabel(f'Czas wykonywania skryptu magazynowego: {main.get("warehouse_time")}'))
+        self.layout.addWidget(self.lbl_warehouse)
 
 
 if __name__ == '__main__':
