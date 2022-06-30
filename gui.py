db07ab3810380ca953c42d055707888ce2d014fa
klaudia <klaudiamiekina11@gmail.com>
2022-06-30 18:56:35 +0200

    gui.py jak user wybierze dwa pliki, czysci layout i dodaje statystyki - u mnie blad connect

--- gui.py
+++ gui.py
@@ -87,6 +87,7 @@ class FirstWindow(QMainWindow):
         self.btn_load_trucks.hide()
         self.btn_load_parcels.hide()
         self.btn_start.hide()
+        # self.window_title('Statystyki')
         self.start()
 
     def start(self):
@@ -99,7 +100,7 @@ class FirstWindow(QMainWindow):
         self.layout.addWidget(lbl_trucks_import)
         lbl_parcels_import = QLabel(f'Czas pobierania pliku .json z paczkami: {main.get("parcels_import_time"):.5f}')
         self.layout.addWidget(lbl_parcels_import)
-        lbl_parcels_send = QLabel(f'Liczba wysłanych paczek: {main.get("parcels_send"):.d}')
+        lbl_parcels_send = QLabel(f'Liczba wysłanych paczek: {main.get("parcels_send"):d}')
         self.layout.addWidget(lbl_parcels_send)
         lbl_trucks_send = QLabel(f'Liczba wysłanych ciężarówek: {main.get("parcels_send"):d}')
         self.layout.addWidget(lbl_trucks_send)
